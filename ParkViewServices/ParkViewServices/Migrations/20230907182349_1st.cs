b4cd14915e471e5ce176b0cdccbf5b9e587f6f32
manavbhatia08 <manavg0821@gmail.com>
2023-09-08 03:48:26 +0530

    my2ndlastcommit

---
+++ ParkViewServices/ParkViewServices/Migrations/20230907182349_1st.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkViewServices.Migrations
+{
+    public partial class _1st : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
