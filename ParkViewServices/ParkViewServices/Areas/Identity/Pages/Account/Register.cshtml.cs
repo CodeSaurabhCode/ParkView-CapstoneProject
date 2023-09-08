295d84af6f0471d55af5703870b14ec6a2790bbd
Saurabh Kshirsagar <saurabh.kshirsagar@kongsbergdigital.com>
2023-09-08 09:24:22 +0530

    pushed final changes

--- ParkViewServices/ParkViewServices/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ParkViewServices/ParkViewServices/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -141,7 +141,7 @@ namespace ParkViewServices.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Explore");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
--- ParkViewServices/ParkViewServices/ViewModel/ReviewBookingViewModel.cs
+++ ParkViewServices/ParkViewServices/ViewModel/ReviewBookingViewModel.cs
@@ -9,6 +9,7 @@ namespace ParkViewServices.ViewModel
         public Hotel Hotel { get; set; }
         public Booking Booking { get; set; }
         public decimal? TotalAmount { get; set; } 
+        public decimal? GSTAmount { get; set; }
         public int TotalNights { get; set; }  
         public HotelSingleImage Images { get; set; }    
         public ApplicationUser? User { get; set; }
